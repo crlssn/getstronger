2f3a9449767b837bb039bb3ba887a2b997e0e0c8
Christian Carlsson <hello@crlssn.com>
2024-08-16 16:16:57 +0100

    checkpoint

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module getstronger
+module github.com/crlssn/getstronger
 
 go 1.23.0
 
--- go/pkg/pb/api/v1/apiv1connect/auth.connect.go
+++ go/pkg/pb/api/v1/apiv1connect/auth.connect.go
@@ -8,7 +8,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "getstronger/go/pkg/pb/api/v1"
+	v1 "github.com/crlssn/getstronger/go/pkg/pb/api/v1"
 	http "net/http"
 	strings "strings"
 )
