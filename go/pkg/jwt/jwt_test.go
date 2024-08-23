a0a76a480023e9b302c9271af73cbfa828602891
Christian Carlsson <hello@crlssn.com>
2024-08-23 16:17:01 +0100

    checkpoint

--- go/pkg/jwt/jwt_test.go
+++ go/pkg/jwt/jwt_test.go
@@ -3,10 +3,13 @@ package jwt
 import (
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func TestGenerateTokens(t *testing.T) {
 	userID := "123"
+	now := time.Now().UTC()
+
 	m := NewManager([]byte("access_key"), []byte("refresh_key"))
 
 	accessToken, err := m.CreateToken(userID, TokenTypeAccess)
@@ -20,10 +23,14 @@ func TestGenerateTokens(t *testing.T) {
 	claims, err := m.ClaimsFromToken(accessToken, TokenTypeAccess)
 	require.NoError(t, err)
 	require.Equal(t, userID, claims.UserID)
+	require.True(t, claims.ExpiresAt.After(now.Add(expiryTimeAccess-time.Second)))
+	require.True(t, claims.ExpiresAt.Before(now.Add(expiryTimeAccess+time.Second)))
 
 	claims, err = m.ClaimsFromToken(refreshToken, TokenTypeRefresh)
 	require.NoError(t, err)
 	require.Equal(t, userID, claims.UserID)
+	require.True(t, claims.ExpiresAt.After(now.Add(expiryTimeRefresh-time.Second)))
+	require.True(t, claims.ExpiresAt.Before(now.Add(expiryTimeRefresh+time.Second)))
 
 	_, err = m.CreateToken(userID, "")
 	require.Error(t, err)
