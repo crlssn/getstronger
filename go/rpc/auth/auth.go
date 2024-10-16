820b6afe8b844be37d9a271c8c9a5087a6e049a2
Christian Carlsson <hello@crlssn.com>
2024-10-16 14:51:23 +0100

    checkpoint

--- go/rpc/auth/auth.go
+++ go/rpc/auth/auth.go
@@ -1,15 +1,18 @@
 package auth
 
 import (
-	"connectrpc.com/connect"
 	"context"
 	"errors"
+	"strings"
+
+	"connectrpc.com/connect"
+	"go.uber.org/zap"
+
 	"github.com/crlssn/getstronger/go/pkg/jwt"
 	v1 "github.com/crlssn/getstronger/go/pkg/pb/api/v1"
 	"github.com/crlssn/getstronger/go/pkg/pb/api/v1/apiv1connect"
 	"github.com/crlssn/getstronger/go/pkg/repos"
-	"go.uber.org/zap"
-	"strings"
+	"github.com/crlssn/getstronger/go/pkg/xzap"
 )
 
 var _ apiv1connect.AuthServiceHandler = (*handler)(nil)
@@ -25,6 +28,8 @@ func NewHandler(log *zap.Logger, repo *repos.Auth, jwt *jwt.Manager) apiv1connec
 }
 
 func (h *handler) Signup(ctx context.Context, req *connect.Request[v1.SignupRequest]) (*connect.Response[v1.SignupResponse], error) {
+	log := h.log.With(xzap.FieldRPC(apiv1connect.AuthServiceSignupProcedure))
+
 	email := strings.ReplaceAll(req.Msg.Email, " ", "")
 	if !strings.Contains(email, "@") {
 		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid email"))
@@ -32,43 +37,45 @@ func (h *handler) Signup(ctx context.Context, req *connect.Request[v1.SignupRequ
 
 	if err := h.repo.Insert(ctx, email, req.Msg.Password); err != nil {
 		if errors.Is(err, repos.ErrAuthEmailExists) {
-			h.log.Warn("email already exists")
+			log.Warn("email already exists")
 			// Do not leak registered emails.
 			return connect.NewResponse(&v1.SignupResponse{}), nil
 		}
 
-		h.log.Error("insert failed", zap.Error(err))
+		log.Error("insert failed", zap.Error(err))
 		return nil, connect.NewError(connect.CodeInternal, errors.New(""))
 	}
 
-	h.log.Info("user signed up")
+	log.Info("user signed up")
 	return connect.NewResponse(&v1.SignupResponse{}), nil
 }
 
 func (h *handler) Login(ctx context.Context, req *connect.Request[v1.LoginRequest]) (*connect.Response[v1.LoginResponse], error) {
+	log := h.log.With(xzap.FieldRPC(apiv1connect.AuthServiceLoginProcedure))
+
 	if err := h.repo.CompareEmailAndPassword(ctx, req.Msg.Email, req.Msg.Password); err != nil {
 		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid credentials"))
 	}
 
 	auth, err := h.repo.FromEmail(ctx, req.Msg.Email)
 	if err != nil {
-		h.log.Error("fetch failed", zap.Error(err))
+		log.Error("fetch failed", zap.Error(err))
 		return nil, connect.NewError(connect.CodeInternal, errors.New(""))
 	}
 
 	accessToken, err := h.jwt.CreateToken(auth.ID, jwt.TokenTypeAccess)
 	if err != nil {
-		h.log.Error("token generation failed", zap.Error(err))
+		log.Error("token generation failed", zap.Error(err))
 		return nil, connect.NewError(connect.CodeInternal, errors.New(""))
 	}
 
 	refreshToken, err := h.jwt.CreateToken(auth.ID, jwt.TokenTypeRefresh)
 	if err != nil {
-		h.log.Error("token generation failed", zap.Error(err))
+		log.Error("token generation failed", zap.Error(err))
 		return nil, connect.NewError(connect.CodeInternal, errors.New(""))
 	}
 
-	h.log.Info("logged in")
+	log.Info("logged in")
 	return connect.NewResponse(&v1.LoginResponse{
 		AccessToken:  accessToken,
 		RefreshToken: refreshToken,
@@ -76,15 +83,17 @@ func (h *handler) Login(ctx context.Context, req *connect.Request[v1.LoginReques
 }
 
 func (h *handler) RefreshToken(ctx context.Context, req *connect.Request[v1.RefreshTokenRequest]) (*connect.Response[v1.RefreshTokenResponse], error) {
+	log := h.log.With(xzap.FieldRPC(apiv1connect.AuthServiceRefreshTokenProcedure))
+
 	claims, err := h.jwt.ClaimsFromToken(req.Msg.RefreshToken, jwt.TokenTypeRefresh)
 	if err != nil {
-		h.log.Error("token parsing failed", zap.Error(err))
+		log.Error("token parsing failed", zap.Error(err))
 		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("invalid refresh token"))
 	}
 
 	accessToken, err := h.jwt.CreateToken(claims.UserID, jwt.TokenTypeAccess)
 	if err != nil {
-		h.log.Error("token generation failed", zap.Error(err))
+		log.Error("token generation failed", zap.Error(err))
 		return nil, connect.NewError(connect.CodeInternal, errors.New(""))
 	}
 
