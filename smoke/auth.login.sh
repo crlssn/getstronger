7a7dee6aa80f726dcea8808513f872dec776f29c
Christian Carlsson <hello@crlssn.com>
2024-10-17 12:46:57 +0100

    checkpoint

--- smoke/auth.login.sh
+++ smoke/auth.login.sh
@@ -2,3 +2,6 @@ curl \
   --header "Content-Type: application/json" \
   --data '{"email": "hello@crlssn.com", "password": "123456"}' \
   http://localhost:8080/api.v1.AuthService/Login
+
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMzMxNDllNDUtNzY4Ny00YTllLTliMTMtYzUxNWM5N2YwNGM0Iiwic3ViIjoicmVmcmVzaF90b2tlbiIsImV4cCI6MTczMTc1NzU0MCwiaWF0IjoxNzI5MTY1NTQwfQ.JrnuXLqZxqWTDfcqioranNSe67dE6XfU4NZ7YWTip3s
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMzMxNDllNDUtNzY4Ny00YTllLTliMTMtYzUxNWM5N2YwNGM0Iiwic3ViIjoicmVmcmVzaF90b2tlbiIsImV4cCI6MTczMTc1NzU2MywiaWF0IjoxNzI5MTY1NTYzfQ.WJAkz_WIqqR7GIz3LqREQSmQVDx9ViYFvn20rPtAhXE
