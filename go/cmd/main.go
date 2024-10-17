85a53190b5f4ba56089e6c34905cd64d80d6831d
Christian Carlsson <hello@crlssn.com>
2024-10-17 12:34:02 +0100

    checkpoint

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/bufbuild/protovalidate-go"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
@@ -21,7 +19,6 @@ func main() {
 func options() []fx.Option {
 	return []fx.Option{
 		jwt.Module(),
-		//interceptors.NewModule(),
 		rpc.NewModule(),
 		fx.Provide(
 			func() db.Options {
@@ -33,40 +30,11 @@ func options() []fx.Option {
 					Database: "postgres",
 				}
 			},
-			//func(interceptors []interceptors.Interceptor) []grpc.ServerOption {
-			//	var opts []grpc.ServerOption
-			//	for _, i := range interceptors {
-			//		opts = append(opts, grpc.UnaryInterceptor(i.Unary()))
-			//		opts = append(opts, grpc.StreamInterceptor(i.Stream()))
-			//	}
-			//	return opts
-			//},
-			//func() *jwt.Manager {
-			//	return jwt.NewManager([]byte("access-key"), []byte("refresh-key"))
-			//},
-			//func() (net.Listener, error) {
-			//	return net.Listen("tcp", ":8080")
-			//},
-			//interceptors.NewAuthInterceptor,
 			db.New,
 			zap.NewNop,
-			//auth.NewHandler,
-			http.NewServeMux,
 			repos.NewAuth,
 			grpc.NewServer,
 			protovalidate.New,
-		), //fx.Invoke(
-		//	//func(server *grpc.Server, listener net.Listener, handler apiv1connect.AuthServiceHandler) error {
-		//	//	return server.Serve(listener)
-		//	//},
-		//	func(mux *http.ServeMux, handlers []rpc.Handler, opts []connect.HandlerOption) error {
-		//		for _, h := range handlers {
-		//			mux.Handle(h())
-		//		}
-		//		mux.Handle(apiv1connect.NewAuthServiceHandler(auth, opts...))
-		//		return http.ListenAndServe(":8080", h2c.NewHandler(mux, &http2.Server{}))
-		//	},
-		//),
-
+		),
 	}
 }
