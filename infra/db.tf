50442cc01e07979f3a49458204cf614d90965b2d
Christian Carlsson <hello@crlssn.com>
2024-11-12 15:58:14 +0000

    checkpoint

--- infra/db.tf
+++ infra/db.tf
@@ -24,7 +24,7 @@ resource "aws_db_instance" "db" {
 
 # Optional: Create a DB subnet group if you don't have one already
 resource "aws_db_subnet_group" "default" {
-  name       = "db-subnet-group"
+  name       = "db-subnet-group-3"
   subnet_ids = ["subnet-0977de5206e697577", "subnet-040d4c7a3aaa9a63d", "subnet-0cf0e0b715c1ec540"] # Replace with your subnet IDs
 
   tags = {
@@ -34,7 +34,7 @@ resource "aws_db_subnet_group" "default" {
 
 # Optional: Create a security group if you don't have one already
 resource "aws_security_group" "default" {
-  name        = "db-security-group-2"
+  name        = "db-security-group-3"
   description = "Allow DB access"
   vpc_id      = "vpc-016eba058ed193190" # Replace with your VPC ID
 
