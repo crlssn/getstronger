9816a132415ff2cc4b023109733d5721cd6b529d
Christian Carlsson <hello@crlssn.com>
2024-11-12 17:24:06 +0000

    test with gh actions

---
+++ infra/s3.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "vue_js_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_website_configuration" "vue_js_bucket" {
+  bucket = aws_s3_bucket.vue_js_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_access" {
+  bucket = aws_s3_bucket.vue_js_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = ["${aws_s3_bucket.vue_js_bucket.arn}/*"]
+      }
+    ]
+  })
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.vue_js_bucket.bucket
+}
+
+output "bucket_website_endpoint" {
+  value = aws_s3_bucket.vue_js_bucket.website_endpoint
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -8,3 +8,15 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "vue-js-app"
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "eu-west-2"
+}
