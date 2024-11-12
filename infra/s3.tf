78e8730b4789c523004091e6ff00a06aba432fee
Christian Carlsson <hello@crlssn.com>
2024-11-12 19:22:43 +0000

    public access

--- infra/s3.tf
+++ infra/s3.tf
@@ -18,6 +18,15 @@ resource "aws_s3_bucket_website_configuration" "vue_js_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.vue_js_bucket.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "public_access" {
   bucket = aws_s3_bucket.vue_js_bucket.id
 
