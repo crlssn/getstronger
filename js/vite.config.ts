0857183762c26b64c205503d987d9f3a80bfbd98
Christian Carlsson <hello@crlssn.com>
2024-11-13 12:52:53 +0000

    disable https

--- js/vite.config.ts
+++ js/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import fs from "node:fs";
+// import fs from "node:fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,10 +14,10 @@ export default defineConfig({
     vueDevTools(),
   ],
   server: {
-    https: {
-      key: fs.readFileSync('./../.secrets/localhost.key'),
-      cert: fs.readFileSync('./../.secrets/localhost.crt'),
-    },
+    // https: {
+    //   key: fs.readFileSync('./../.secrets/localhost.key'),
+    //   cert: fs.readFileSync('./../.secrets/localhost.crt'),
+    // },
     port: 5173, // Optional: set the port you want to use
   },
   resolve: {
