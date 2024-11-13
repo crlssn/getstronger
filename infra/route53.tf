d0f29966a153b7b0f16c49db8791c2d707d0d0a3
Christian Carlsson <hello@crlssn.com>
2024-11-13 19:55:11 +0000

    add ns and soa records

--- infra/route53.tf
+++ infra/route53.tf
@@ -2,6 +2,29 @@ resource "aws_route53_zone" "getstronger_pro" {
   name = "getstronger.pro"
 }
 
+# NS Record
+resource "aws_route53_record" "ns_getstronger_pro" {
+  zone_id = aws_route53_zone.getstronger_pro.zone_id
+  name    = "getstronger.pro"
+  type    = "NS"
+  ttl     = 172800
+  records = [
+    "ns-1470.awsdns-55.org.",
+    "ns-1903.awsdns-45.co.uk.",
+    "ns-278.awsdns-34.com.",
+    "ns-708.awsdns-24.net."
+  ]
+}
+
+# SOA Record
+resource "aws_route53_record" "soa_getstronger_pro" {
+  zone_id = aws_route53_zone.getstronger_pro.zone_id
+  name    = "getstronger.pro"
+  type    = "SOA"
+  ttl     = 900
+  records = ["ns-1470.awsdns-55.org. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
+}
+
 # Elastic IP for EC2 instance
 resource "aws_eip" "ec2_instance" {
   instance = aws_instance.backend.id
