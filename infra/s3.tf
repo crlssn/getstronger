9e8e9a346679e04f32c3fcc019c6674ed5376101
Christian Carlsson <hello@crlssn.com>
2024-11-13 13:03:52 +0000

    checkpoint

--- infra/s3.tf
+++ infra/s3.tf
@@ -18,30 +18,30 @@ resource "aws_s3_bucket" "vue_js_bucket" {
 # #   }
 # # }
 # #
-# # resource "aws_s3_bucket_public_access_block" "public_access_block" {
-# #   bucket = aws_s3_bucket.vue_js_bucket.id
-# #
-# #   block_public_acls       = false
-# #   block_public_policy     = false
-# #   ignore_public_acls      = false
-# #   restrict_public_buckets = false
-# # }
-# #
-resource "aws_s3_bucket_policy" "public_access" {
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.vue_js_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource  = ["${aws_s3_bucket.vue_js_bucket.arn}/*"]
-      }
-    ]
-  })
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
+
+# resource "aws_s3_bucket_policy" "public_access" {
+#   bucket = aws_s3_bucket.vue_js_bucket.id
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = ["s3:GetObject"]
+#         Resource  = ["${aws_s3_bucket.vue_js_bucket.arn}/*"]
+#       }
+#     ]
+#   })
+# }
 # #
 # # output "bucket_name" {
 # #   value = aws_s3_bucket.vue_js_bucket.bucket
