5f016baa0a29098d763ab81ec0329cf3803aaef7
Christian Carlsson <hello@crlssn.com>
2024-11-13 19:46:55 +0000

    fmt

--- infra/acm.tf
+++ infra/acm.tf
@@ -2,8 +2,8 @@ module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "~> 4.0"
 
-  domain_name  = "getstronger.pro"
-  zone_id      = aws_route53_zone.getstronger_pro.zone_id
+  domain_name = "getstronger.pro"
+  zone_id     = aws_route53_zone.getstronger_pro.zone_id
 
   validation_method = "DNS"
 
