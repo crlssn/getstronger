beff0d0cd6a5985f98ae64be1002f0439082a231
Christian Carlsson <hello@crlssn.com>
2024-11-13 11:17:00 +0000

    checkpoint

--- infra/main.tf
+++ infra/main.tf
@@ -6,12 +6,32 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket         = "getstronger-terraform-state" # Replace with your bucket name
+    key            = "terraform/state.tfstate"     # Define a path to store the state file within the bucket
+    region         = "eu-west-2"                # Replace with your AWS region
+    dynamodb_table = "terraform-lock-table"        # The DynamoDB table to use for locking
+    encrypt        = true                          # Enable encryption at rest
+    # shared_credentials_files = "~/.aws/credentials"
+  }
+
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  region = var.aws_region
 }
+
+# resource "aws_dynamodb_table" "terraform_lock" {
+#   name         = "terraform-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
 
 # resource "aws_instance" "app_server" {
 #   ami           = "ami-830c94e3"
