a1a3432deaa25fb17cff91bf4cc280f20065a490
Christian Carlsson <hello@crlssn.com>
2024-11-13 11:25:26 +0000

    checkpoint

--- infra/main.tf
+++ infra/main.tf
@@ -1,16 +1,16 @@
-resource "aws_s3_bucket" "terraform_state_for_andromeda" {
-  bucket = "getstronger-terraform-state"
-}
-
-resource "aws_dynamodb_table" "terraform_lock_state" {
-  name         = "dynamoDB_to_lock_terraform_state"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_s3_bucket" "terraform_state_for_andromeda" {
+#   bucket = "getstronger-terraform-state"
+# }
+#
+# resource "aws_dynamodb_table" "terraform_lock_state" {
+#   name         = "dynamoDB_to_lock_terraform_state"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
 
 terraform {
   required_providers {
@@ -19,14 +19,15 @@ terraform {
       version = "~> 4.16"
     }
   }
+  backend "s3" {}
 
-  backend "s3" {
-    bucket = "getstronger-terraform-state" # Replace with your bucket name
-    key = "state.tfstate"     # Define a path to store the state file within the bucket
-    region = "eu-west-2"                # Replace with your AWS region
-    dynamodb_table = "terraform-lock-table"        # The DynamoDB table to use for locking
-    encrypt = true                          # Enable encryption at rest
-  }
+  # backend "s3" {
+  #   bucket = "getstronger-terraform-state" # Replace with your bucket name
+  #   key = "state.tfstate"     # Define a path to store the state file within the bucket
+  #   region = "eu-west-2"                # Replace with your AWS region
+  #   dynamodb_table = "terraform-lock-table"        # The DynamoDB table to use for locking
+  #   encrypt = true                          # Enable encryption at rest
+  # }
 
   required_version = ">= 1.2.0"
 }
