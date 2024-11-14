82c3aec676e1c36bed104949c3fc46d355f79a66
Christian Carlsson <hello@crlssn.com>
2024-11-14 13:13:38 +0000

    enable acm

--- infra/acm.tf
+++ infra/acm.tf
@@ -1,20 +1,20 @@
-# module "acm" {
-#   source  = "terraform-aws-modules/acm/aws"
-#   version = "~> 4.0"
-#
-#   domain_name = "getstronger.pro"
-#   zone_id     = aws_route53_zone.getstronger_pro.zone_id
-#
-#   validation_method = "DNS"
-#
-#   subject_alternative_names = [
-#     "www.getstronger.pro",
-#     "api.getstronger.pro",
-#   ]
-#
-#   wait_for_validation = false
-#
-#   tags = {
-#     Name = "getstronger.pro"
-#   }
-# }
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  domain_name = "getstronger.pro"
+  zone_id     = aws_route53_zone.getstronger_pro.zone_id
+
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "www.getstronger.pro",
+    "api.getstronger.pro",
+  ]
+
+  wait_for_validation = false
+
+  tags = {
+    Name = "getstronger.pro"
+  }
+}
