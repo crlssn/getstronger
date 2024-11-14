09481429e713393ad3189a4d2d43995da6173db8
Christian Carlsson <hello@crlssn.com>
2024-11-14 20:15:41 +0000

    feat: enable ssl (#12)

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/bufbuild/protovalidate-go"
@@ -16,6 +17,10 @@ import (
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(fmt.Errorf("failed to load .env file: %w", err))
+	}
+
 	fx.New(options()...).Run()
 }
 
@@ -37,7 +42,6 @@ func options() []fx.Option {
 			zap.NewDevelopment,
 			repo.New,
 			grpc.NewServer,
-			godotenv.Load,
 			protovalidate.New,
 		),
 	}
--- infra/route53.tf
+++ infra/route53.tf
@@ -20,7 +20,24 @@ resource "aws_route53_record" "api_getstronger_pro" {
 resource "aws_route53_record" "www_getstronger_pro" {
   zone_id = aws_route53_zone.getstronger_pro.zone_id
   name    = "www.getstronger.pro"
-  type    = "CNAME"
+  type    = "A" # Change type to "A" for alias records with CloudFront
+
+  alias {
+    name                   = aws_cloudfront_distribution.www_getstronger_pro_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www_getstronger_pro_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+  # zone_id = aws_route53_zone.getstronger_pro.zone_id
+  # name    = "www.getstronger.pro"
+  # type    = "CNAME"
+  # ttl     = 300
+  # records = [aws_s3_bucket.www_getstronger_pro.website_endpoint]
+}
+
+resource "aws_route53_record" "ssh_getstronger_pro" {
+  zone_id = aws_route53_zone.getstronger_pro.zone_id
+  name    = "ssh.getstronger.pro"
+  type    = "A"
   ttl     = 300
-  records = [aws_s3_bucket.www_getstronger_pro.website_endpoint]
+  records = [aws_eip.ec2_instance.public_ip]
 }
