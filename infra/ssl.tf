e2642f15c34142edbe71b3b0567b1336a534a1f5
Christian Carlsson <hello@crlssn.com>
2024-11-14 14:38:43 +0000

    aws_cloudfront_distribution

--- infra/ssl.tf
+++ infra/ssl.tf
@@ -32,43 +32,44 @@ resource "aws_acm_certificate_validation" "s3_cert_validation" {
 }
 
 # # Create CloudFront distribution for the S3 bucket
-# resource "aws_cloudfront_distribution" "www_getstronger_pro_distribution" {
-#   origin {
-#     domain_name = aws_s3_bucket.www_getstronger_pro.bucket_domain_name
-#     origin_id   = "S3-origin"
-#   }
-#
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   default_root_object = "index.html"
-#
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = "S3-origin"
-#
-#     viewer_protocol_policy = "redirect-to-https"
-#     forwarded_values {
-#       query_string = false
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-#   }
-#
-#   # Associate SSL certificate
-#   viewer_certificate {
-#     acm_certificate_arn = aws_acm_certificate.www_getstronger_pro_ssl_cert.arn
-#     ssl_support_method  = "sni-only"
-#   }
-#
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
-#
-#   tags = {
-#     Name = "S3CloudFrontDistribution"
-#   }
-# }
+resource "aws_cloudfront_distribution" "www_getstronger_pro_distribution" {
+  provider = aws.us_east_1
+  origin {
+    domain_name = aws_s3_bucket.www_getstronger_pro.bucket_domain_name
+    origin_id   = "S3-origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  # Associate SSL certificate
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.www_getstronger_pro_ssl_cert.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "S3CloudFrontDistribution"
+  }
+}
