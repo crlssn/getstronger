1c6a900c4fdb20013a2189570340662f159e291b
Christian Carlsson <hello@crlssn.com>
2024-11-14 13:50:33 +0000

    use for each

--- infra/ssl.tf
+++ infra/ssl.tf
@@ -6,10 +6,12 @@ resource "aws_acm_certificate" "www_getstronger_pro_ssl_cert" {
 
 # # Validate the certificate
 resource "aws_route53_record" "s3_ssl_cert_validation" {
-  name    = aws_acm_certificate.www_getstronger_pro_ssl_cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.www_getstronger_pro_ssl_cert.domain_validation_options[0].resource_record_type
+  for_each = { for dvo in aws_acm_certificate.www_getstronger_pro_ssl_cert.domain_validation_options : dvo.domain_name => dvo }
+
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
   zone_id = aws_route53_zone.getstronger_pro.zone_id
-  records = [aws_acm_certificate.www_getstronger_pro_ssl_cert.domain_validation_options[0].resource_record_value]
+  records = [each.value.resource_record_value]
   ttl     = 60
 }
 
