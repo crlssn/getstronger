396ffd03ff9f574b5f821b2023dd9b3f49816936
Christian Carlsson <hello@crlssn.com>
2024-11-14 14:37:36 +0000

    add provider to val

--- infra/ssl.tf
+++ infra/ssl.tf
@@ -22,6 +22,7 @@ resource "aws_route53_record" "s3_ssl_cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "s3_cert_validation" {
+  provider = aws.us_east_1
   certificate_arn         = aws_acm_certificate.www_getstronger_pro_ssl_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.s3_ssl_cert_validation : record.fqdn]
   depends_on = [
