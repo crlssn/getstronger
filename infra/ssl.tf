8e70ea72d885a0c45c0718f13b2a5f1596568026
Christian Carlsson <hello@crlssn.com>
2024-11-14 17:46:39 +0000

    enable aws_cloudfront_distribution

--- infra/ssl.tf
+++ infra/ssl.tf
@@ -116,54 +116,54 @@ resource "aws_acm_certificate_validation" "api_getstronger_pro_cert_validation" 
   ]
 }
 
-# # Create CloudFront distribution for EC2 instance
-# resource "aws_cloudfront_distribution" "api_getstronger_pro_distribution" {
-#   provider = aws.us_east_1
-#
-#   origin {
-#     domain_name = "api.getstronger.pro"
-#     origin_id   = "EC2-origin"
-#
-#     custom_origin_config {
-#       http_port              = 80
-#       https_port             = 443
-#       origin_protocol_policy = "https-only"
-#       origin_ssl_protocols = ["TLSv1.2"]
-#     }
-#   }
-#
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   default_root_object = "/"
-#
-#   default_cache_behavior {
-#     allowed_methods = ["GET", "HEAD"]
-#     cached_methods = ["GET", "HEAD"]
-#     target_origin_id = "EC2-origin"
-#
-#     viewer_protocol_policy = "redirect-to-https"
-#     forwarded_values {
-#       query_string = false
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-#   }
-#
-#   # Associate SSL certificate
-#   viewer_certificate {
-#     acm_certificate_arn = aws_acm_certificate.api_getstronger_pro_ssl_cert.arn
-#     ssl_support_method  = "sni-only"
-#   }
-#
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
-#
-#   tags = {
-#     Name = "EC2CloudFrontDistribution"
-#   }
-# }
+# Create CloudFront distribution for EC2 instance
+resource "aws_cloudfront_distribution" "api_getstronger_pro_distribution" {
+  provider = aws.us_east_1
+
+  origin {
+    domain_name = "api.getstronger.pro"
+    origin_id   = "EC2-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "/"
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "EC2-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  # Associate SSL certificate
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.api_getstronger_pro_ssl_cert.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "EC2CloudFrontDistribution"
+  }
+}
 
