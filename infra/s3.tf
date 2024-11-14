6e41f7832dd674968119cadd07c7d79bb063ec16
Christian Carlsson <hello@crlssn.com>
2024-11-14 12:55:15 +0000

    Revert "disable public access block"
    This reverts commit 0f5968cdbf022da1f399728fa8a1e4d276fcfe6e.

--- infra/s3.tf
+++ infra/s3.tf
@@ -14,14 +14,14 @@ resource "aws_s3_bucket_website_configuration" "vue_js_bucket" {
   }
 }
 
-# resource "aws_s3_bucket_public_access_block" "public_access_block" {
-#   bucket = aws_s3_bucket.www_getstronger_pro.id
-#
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
-# }
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.www_getstronger_pro.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 resource "aws_s3_bucket_policy" "public_access" {
   bucket = aws_s3_bucket.www_getstronger_pro.id
