c715e0a9d7f19aa4bed464f09bf64348f3d5bcb5
Christian Carlsson <hello@crlssn.com>
2024-11-14 14:25:14 +0000

    enable aws_acm_certificate_validation

--- infra/ssl.tf
+++ infra/ssl.tf
@@ -21,12 +21,15 @@ resource "aws_route53_record" "s3_ssl_cert_validation" {
   ttl     = 60
 }
 
-# resource "aws_acm_certificate_validation" "s3_cert_validation" {
-#   certificate_arn         = aws_acm_certificate.www_getstronger_pro_ssl_cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.s3_ssl_cert_validation : record.fqdn]
-#   depends_on = [aws_route53_record.s3_ssl_cert_validation]
-# }
-#
+resource "aws_acm_certificate_validation" "s3_cert_validation" {
+  certificate_arn         = aws_acm_certificate.www_getstronger_pro_ssl_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.s3_ssl_cert_validation : record.fqdn]
+  depends_on = [
+    aws_acm_certificate.www_getstronger_pro_ssl_cert,
+    aws_route53_record.s3_ssl_cert_validation
+  ]
+}
+
 # # Create CloudFront distribution for the S3 bucket
 # resource "aws_cloudfront_distribution" "www_getstronger_pro_distribution" {
 #   origin {
