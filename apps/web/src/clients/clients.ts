0f3685fc51763f6d9a9879a194895c68749984fa
Christian Carlsson <hello@crlssn.com>
2024-11-17 16:54:48 +0000

    refact: use interceptor to refresh access token (#24)

--- apps/web/src/clients/clients.ts
+++ apps/web/src/clients/clients.ts
@@ -1,21 +1,8 @@
 import {createConnectTransport} from "@connectrpc/connect-web";
-import {createClient, type Interceptor} from "@connectrpc/connect";
+import {createClient} from "@connectrpc/connect";
 import {AuthService} from "@/pb/api/v1/auth_connect";
 import {ExerciseService} from "@/pb/api/v1/exercise_connect";
-import {useAuthStore} from "@/stores/auth";
-
-const logger: Interceptor = (next) => async (req) => {
-  console.log(`sending message to ${req.url}`);
-  return await next(req);
-};
-
-const auth: Interceptor = (next) => async (req) => {
-  const authStore = useAuthStore();
-  if (authStore.accessToken) {
-    req.header.set("Authorization", `Bearer ${authStore.accessToken}`);
-  }
-  return next(req);
-};
+import {auth, logger} from "@/clients/interceptors";
 
 const transport = createConnectTransport({
   baseUrl: import.meta.env.VITE_API_URL,
