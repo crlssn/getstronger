2378783e5ed5566ec2a582a6064d38911b99e37f
Christian Carlsson <hello@crlssn.com>
2024-11-17 16:52:07 +0000

    refact: use interceptor to refresh access token

---
+++ apps/web/src/clients/interceptors.ts
@@ -0,0 +1,30 @@
+import type {Interceptor} from "@connectrpc/connect";
+import {useAuthStore} from "@/stores/auth";
+import {Code, ConnectError} from "@connectrpc/connect";
+import {RefreshAccessTokenOrLogout} from "@/jwt/jwt";
+
+export const logger: Interceptor = (next) => async (req) => {
+  console.log(`sending message to ${req.url}`);
+  return next(req);
+};
+
+export const auth: Interceptor = (next) => async (req) => {
+  const authStore = useAuthStore();
+  try {
+    req.header.set("Authorization", `Bearer ${authStore.accessToken}`);
+    return next(req);
+  } catch (error) {
+    if (!(error instanceof ConnectError)) {
+      throw error;
+    }
+
+    if (error.code !== Code.Unauthenticated) {
+      throw error;
+    }
+
+    console.log("refreshing access token to attempt request again");
+    await RefreshAccessTokenOrLogout();
+    req.header.set("Authorization", `Bearer ${authStore.accessToken}`);
+    return next(req);
+  }
+};
