4528e6647172fca592cb76462a65b3962cebda06
Christian Carlsson <hello@crlssn.com>
2024-11-21 19:53:51 +0000

    format

--- apps/web/src/pkg/datetime/datetime.ts
+++ apps/web/src/pkg/datetime/datetime.ts
@@ -1,6 +1,6 @@
 import { DateTime } from 'luxon'
 
-export const formatToCompactDateTime = (date: Date|undefined): string => {
+export const formatToCompactDateTime = (date: Date | undefined): string => {
   if (!date) return ''
   return DateTime.fromJSDate(date).toFormat('EEE dd LLL HH:mm')
 }
