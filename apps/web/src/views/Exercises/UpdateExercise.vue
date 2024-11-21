0e63574aef3f089046a83e646856da0822ab65c7
Christian Carlsson <hello@crlssn.com>
2024-11-21 20:58:53 +0000

    format

--- apps/web/src/views/Exercises/UpdateExercise.vue
+++ apps/web/src/views/Exercises/UpdateExercise.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-import {
-  Exercise,
-  GetExerciseRequest,
-  UpdateExerciseRequest,
-} from '@/pb/api/v1/exercise_pb'
+import { Exercise, GetExerciseRequest, UpdateExerciseRequest } from '@/pb/api/v1/exercise_pb'
 import AppButton from '@/components/AppButton.vue'
 import { onMounted, ref } from 'vue'
 import { ExerciseClient } from '@/clients/clients'
