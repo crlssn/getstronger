16a23065b0b66136ba13a86c3eb1620766c315da
Christian Carlsson <hello@crlssn.com>
2024-11-21 13:40:49 +0000

    chore: remove purge field from tailwind config

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: ["./src/**/*.{html,js}"],
+  content: ['./index.html', './src/**/*.{html,vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require('@tailwindcss/forms')],
 }
-
