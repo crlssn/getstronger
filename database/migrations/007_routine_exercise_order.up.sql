150571ac8db13f8bedc2a3fc4e2598a583439620
Christian Carlsson <hello@crlssn.com>
2024-11-23 14:54:33 +0000

    checkpoint

--- database/migrations/007_routine_exercise_order.up.sql
+++ database/migrations/007_routine_exercise_order.up.sql
@@ -1,8 +1,1 @@
-CREATE TABLE getstronger.routine_exercises_sort_order
-(
-    id          UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-    routine_id  UUID             NOT NULL REFERENCES getstronger.routines (id),
-    exercise_id UUID             NOT NULL REFERENCES getstronger.exercises (id),
-    sort_order  INT              NOT NULL,
-    UNIQUE (routine_id, exercise_id)
-);
+ALTER TABLE getstronger.routines ADD COLUMN exercise_order JSONB;
