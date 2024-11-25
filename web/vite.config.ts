c53a02fd0cd893151afbe9344dadbb0d9865b5eb
Christian Carlsson <hello@crlssn.com>
2024-11-25 15:51:09 +0000

    checkpoint

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-// import fs from "node:fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,12 +13,8 @@ export default defineConfig({
     vueDevTools(),
   ],
   server: {
-    // https: {
-    //   key: fs.readFileSync('./../.secrets/localhost.key'),
-    //   cert: fs.readFileSync('./../.secrets/localhost.crt'),
-    // },
     host: '0.0.0.0',
-    port: 5173, // Optional: set the port you want to use
+    port: 5173,
   },
   resolve: {
     alias: {
