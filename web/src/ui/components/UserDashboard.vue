6f8356b6086a3d03e8bc349d44312e6d7302ab38
Christian Carlsson <hello@crlssn.com>
2024-11-25 15:41:23 +0000

    checkpoint

--- web/src/ui/components/UserDashboard.vue
+++ web/src/ui/components/UserDashboard.vue
@@ -17,7 +17,7 @@ import {
   HomeIcon,
   XMarkIcon,
 } from '@heroicons/vue/24/outline'
-import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
 import { RouterView, useRoute } from 'vue-router'
 import NavigationMobile from '@/ui/components/NavigationMobile.vue'
 import { usePageTitleStore } from '@/stores/pageTitle.ts'
@@ -215,7 +215,7 @@ const pageTitleStore = usePageTitleStore()
           <!--                   class="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"-->
           <!--                   placeholder="Search..." type="search" name="search"/>-->
           <!--          </form>-->
-          <div class="flex items-center gap-x-4 lg:gap-x-6">
+<!--          <div class="flex items-center gap-x-4 lg:gap-x-6">-->
             <!--            <button type="button" class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">-->
             <!--              <span class="sr-only">View notifications</span>-->
             <!--              <BellIcon class="h-6 w-6" aria-hidden="true"/>-->
@@ -225,53 +225,54 @@ const pageTitleStore = usePageTitleStore()
             <!--            <div class="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10" aria-hidden="true"/>-->
 
             <!-- Profile dropdown -->
-            <Menu as="div" class="relative">
-              <MenuButton class="-m-1.5 flex items-center p-1.5">
-                <span class="sr-only">Open user menu</span>
-                <img
-                  class="h-8 w-8 rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-                <span class="hidden lg:flex lg:items-center">
-                  <span class="ml-4 text-sm/6 font-semibold text-gray-900" aria-hidden="true"
-                    >Tom Cook</span
-                  >
-                  <ChevronDownIcon class="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
-                </span>
-              </MenuButton>
-              <transition
-                enter-active-class="transition ease-out duration-100"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
-              >
-                <MenuItems
-                  class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
-                >
-                  <MenuItem
-                    v-for="item in userNavigation"
-                    :key="item.name"
-                    v-slot="{ active }"
-                    as="string"
-                  >
-                    <RouterLink
-                      :to="item.href"
-                      :class="[
-                        active ? 'bg-gray-50 outline-none' : '',
-                        'block px-3 py-1 text-sm/6 text-gray-900',
-                      ]"
-                    >
-                      {{ item.name }}
-                    </RouterLink>
-                  </MenuItem>
-                </MenuItems>
-              </transition>
-            </Menu>
-          </div>
+<!--            <Menu as="div" class="relative">-->
+<!--              <MenuButton class="-m-1.5 flex items-center p-1.5">-->
+<!--                <span class="sr-only">Open user menu</span>-->
+<!--                <img-->
+<!--                  class="h-8 w-8 rounded-full bg-gray-50"-->
+<!--                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"-->
+<!--                  alt=""-->
+<!--                />-->
+<!--                <span class="hidden lg:flex lg:items-center">-->
+<!--                  <span class="ml-4 text-sm/6 font-semibold text-gray-900" aria-hidden="true"-->
+<!--                    >Tom Cook</span-->
+<!--                  >-->
+<!--                  <ChevronDownIcon class="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />-->
+<!--                </span>-->
+<!--              </MenuButton>-->
+<!--              <transition-->
+<!--                enter-active-class="transition ease-out duration-100"-->
+<!--                enter-from-class="transform opacity-0 scale-95"-->
+<!--                enter-to-class="transform opacity-100 scale-100"-->
+<!--                leave-active-class="transition ease-in duration-75"-->
+<!--                leave-from-class="transform opacity-100 scale-100"-->
+<!--                leave-to-class="transform opacity-0 scale-95"-->
+<!--              >-->
+<!--                <MenuItems-->
+<!--                  class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"-->
+<!--                >-->
+<!--                  <MenuItem-->
+<!--                    v-for="item in userNavigation"-->
+<!--                    :key="item.name"-->
+<!--                    v-slot="{ active }"-->
+<!--                    as="string"-->
+<!--                  >-->
+<!--                    <RouterLink-->
+<!--                      :to="item.href"-->
+<!--                      :class="[-->
+<!--                        active ? 'bg-gray-50 outline-none' : '',-->
+<!--                        'block px-3 py-1 text-sm/6 text-gray-900',-->
+<!--                      ]"-->
+<!--                    >-->
+<!--                      {{ item.name }}-->
+<!--                    </RouterLink>-->
+<!--                  </MenuItem>-->
+<!--                </MenuItems>-->
+<!--              </transition>-->
+<!--            </Menu>-->
+<!--          </div>-->
         </div>
+        <MagnifyingGlassIcon class="w-6 h-6"/>
       </div>
 
       <main class="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
