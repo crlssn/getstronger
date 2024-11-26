07c841306e911d72f3969eb7145691e984fc423a
Christian Carlsson <hello@crlssn.com>
2024-11-26 14:46:09 +0000

    eslint/sort-imports

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -23,4 +23,14 @@ export default [
   },
 
   skipFormatting,
+
+  {
+    "sort-imports": ["error", {
+      "ignoreCase": false,
+      "ignoreDeclarationSort": false,
+      "ignoreMemberSort": false,
+      "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
+      "allowSeparatedGroups": false
+    }]
+  },
 ]
