865439f58b5221689f04ece2691d3d2ed0850f69
Christian Carlsson <hello@crlssn.com>
2024-12-02 22:47:45 +0000

    format

--- web/src/ui/notifications/ListNotifications.vue
+++ web/src/ui/notifications/ListNotifications.vue
@@ -3,12 +3,12 @@ import type { PaginationRequest } from '@/proto/api/v1/shared_pb.ts'
 
 import { onMounted, ref } from 'vue'
 import { create } from '@bufbuild/protobuf'
+import AppList from '@/ui/components/AppList.vue'
 import { NotificationClient } from '@/clients/clients.ts'
+import AppListItem from '@/ui/components/AppListItem.vue'
 import NotificationUserFollow from '@/ui/components/NotificationUserFollow.vue'
 import NotificationWorkoutComment from '@/ui/components/NotificationWorkoutComment.vue'
 import { ListNotificationsRequestSchema, type Notification } from '@/proto/api/v1/notifications_pb.ts'
-import AppList from '@/ui/components/AppList.vue'
-import AppListItem from '@/ui/components/AppListItem.vue'
 
 const notifications = ref([] as Notification[])
 const pageToken = ref(new Uint8Array(0))
