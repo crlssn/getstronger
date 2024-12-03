29f7f07d326a5d033c7067ff77962b8251db2d4f
Christian Carlsson <hello@crlssn.com>
2024-12-03 15:09:19 +0000

    feat: display followers and followees (#153)

--- web/src/ui/profile/ProfileView.vue
+++ web/src/ui/profile/ProfileView.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
 import router from '@/router/router'
-import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { create } from '@bufbuild/protobuf'
+import { computed, onMounted, ref } from 'vue'
 import { useAuthStore } from '@/stores/auth.ts'
-import { WorkoutClient } from '@/clients/clients'
+import AppList from '@/ui/components/AppList.vue'
 import AppButton from '@/ui/components/AppButton.vue'
+import { type User } from '@/proto/api/v1/shared_pb.ts'
 import CardWorkout from '@/ui/components/CardWorkout.vue'
+import { UserClient, WorkoutClient } from '@/clients/clients'
+import AppListItemLink from '@/ui/components/AppListItemLink.vue'
 import { ListWorkoutsRequestSchema, type Workout } from '@/proto/api/v1/workouts_pb'
+import { ListFolloweesRequestSchema, ListFollowersRequestSchema } from '@/proto/api/v1/users_pb.ts'
 
 const workouts = ref<Workout[]>()
+const followers = ref<User[]>()
+const followees = ref<User[]>()
 const route = useRoute()
 const authStore = useAuthStore()
 
 onMounted(async () => {
-  await fetchWorkouts()
+  await Promise.all([fetchWorkouts(), fetchFollowers(), fetchFollowees()])
 })
 
 const fetchWorkouts = async () => {
@@ -27,12 +33,30 @@ const fetchWorkouts = async () => {
   workouts.value = res.workouts
 }
 
+const fetchFollowers = async () => {
+  const req = create(ListFollowersRequestSchema, {
+    followerId: authStore.userID,
+  })
+  const res = await UserClient.listFollowers(req)
+  followers.value = res.followers
+}
+
+const fetchFollowees = async () => {
+  const req = create(ListFolloweesRequestSchema, {
+    followeeId: authStore.userID,
+  })
+  const res = await UserClient.listFollowees(req)
+  followees.value = res.followees
+}
+
 const tabs = [
   { href: '/profile', name: 'Workouts' },
   { href: '/profile?tab=personal-bests', name: 'Personal Bests' },
   { href: '/profile?tab=follows', name: 'Follows' },
   { href: '/profile?tab=followers', name: 'Followers' },
 ]
+
+const activeTab = computed(() => route.fullPath)
 
 const updateTab = (event: Event) => {
   const target = event.target as HTMLSelectElement
@@ -62,7 +86,7 @@ const updateTab = (event: Event) => {
           v-for="tab in tabs"
           :key="tab.name"
           :value="tab.href"
-          :selected="tab.href === route.fullPath"
+          :selected="tab.href === activeTab"
         >
           {{ tab.name }}
         </option>
@@ -78,7 +102,7 @@ const updateTab = (event: Event) => {
           :key="tab.name"
           :to="tab.href"
           :class="[
-            tab.href === route.fullPath
+            tab.href === activeTab
               ? 'border-gray-200 text-gray-900 bg-white'
               : 'border-transparent text-gray-500 hover:text-gray-700',
             'w-1/4 border border-b-8 py-3.5 text-center text-sm font-semibold rounded-md uppercase',
@@ -89,13 +113,31 @@ const updateTab = (event: Event) => {
       </nav>
     </div>
   </div>
-  <div v-if="route.fullPath === tabs[0].href">
+  <div v-if="activeTab === tabs[0].href">
     <CardWorkout
       v-for="workout in workouts"
       :key="workout.id"
       :workout="workout"
     />
   </div>
+  <AppList v-if="activeTab === tabs[2].href">
+    <AppListItemLink
+      v-for="followee in followees"
+      :key="followee.id"
+      :to="`/users/${followee.id}`"
+    >
+      {{ followee.firstName }} {{ followee.lastName }}
+    </AppListItemLink>
+  </AppList>
+  <AppList v-if="activeTab === tabs[3].href">
+    <AppListItemLink
+      v-for="follower in followers"
+      :key="follower.id"
+      :to="`/users/${follower.id}`"
+    >
+      {{ follower.firstName }} {{ follower.lastName }}
+    </AppListItemLink>
+  </AppList>
 </template>
 
 <style scoped></style>
