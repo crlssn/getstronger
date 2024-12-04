73711930395b93be862a8644c88c98b2a16078ed
Christian Carlsson <hello@crlssn.com>
2024-12-04 15:35:19 +0000

    add docs

--- web/src/http/requests.ts
+++ web/src/http/requests.ts
@@ -6,6 +6,12 @@ import { DeleteExerciseRequestSchema, type DeleteExerciseResponse } from "@/prot
 
 import { AuthClient, ExerciseClient, RoutineClient, WorkoutClient } from "./clients"
 
+/**
+ * Deletes a workout by its ID.
+ *
+ * @param {string} id - The ID of the workout to delete.
+ * @returns {Promise<DeleteWorkoutResponse | void>} A promise that resolves to the response of the delete operation or void if an error occurs.
+ */
 export const deleteWorkout = async (id: string): Promise<DeleteWorkoutResponse | void> => {
   const req = create(DeleteWorkoutRequestSchema, {
     id: id,  
@@ -14,6 +20,12 @@ export const deleteWorkout = async (id: string): Promise<DeleteWorkoutResponse |
   return tryCatch(() => WorkoutClient.delete(req))
 }
 
+/**
+ * Deletes an exercise by its ID.
+ *
+ * @param {string} id - The ID of the exercise to be deleted.
+ * @returns {Promise<DeleteExerciseResponse | void>} A promise that resolves to the response of the delete operation or void if an error occurs.
+ */
 export const deleteExercise = async (id: string): Promise<DeleteExerciseResponse | void> => {
   const req = create(DeleteExerciseRequestSchema, {
     id: id,  
@@ -22,6 +34,12 @@ export const deleteExercise = async (id: string): Promise<DeleteExerciseResponse
   return tryCatch(() => ExerciseClient.delete(req))
 }
 
+/**
+ * Deletes a routine by its ID.
+ *
+ * @param {string} id - The ID of the routine to delete.
+ * @returns {Promise<DeleteRoutineResponse | void>} A promise that resolves to the response of the delete operation or void if an error occurs.
+ */
 export const deleteRoutine = async (id: string): Promise<DeleteRoutineResponse | void> => {
   const req = create(DeleteRoutineRequestSchema, {
     id: id,  
@@ -30,6 +48,13 @@ export const deleteRoutine = async (id: string): Promise<DeleteRoutineResponse |
   return tryCatch(() => RoutineClient.delete(req))
 }
 
+/**
+ * Logs the user in using the provided email and password.
+ *
+ * @param {string} email - The email address of the user.
+ * @param {string} password - The password of the user.
+ * @returns {Promise<LoginResponse | void>} A promise that resolves to the response of the login operation or void if an error occurs.
+ */
 export const login = async (email: string, password: string): Promise<LoginResponse | void> => {
   const req = create(LoginRequestSchema, {
       email: email,
@@ -39,6 +64,14 @@ export const login = async (email: string, password: string): Promise<LoginRespo
   return tryCatch(() => AuthClient.login(req))
 }
 
+/**
+ * Executes an asynchronous function and catches any errors that occur during its execution.
+ * If an error is caught, it displays an alert with the error message.
+ * 
+ * @template T - The type of the resolved value of the promise.
+ * @param {() => Promise<T>} fn - The asynchronous function to be executed.
+ * @returns {Promise<T | void>} - A promise that resolves to the value returned by the function, or void if an error occurs.
+ */
 const tryCatch = async <T>(fn: () => Promise<T>): Promise<T|void> => {
   try {
       return await fn()
