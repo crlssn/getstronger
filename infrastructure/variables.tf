360ee8eda71ca76a253cbca6b81b36f3f1bf4c67
Christian Carlsson <hello@crlssn.com>
2024-12-04 19:50:57 +0000

    outputs

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -20,3 +20,15 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-2"
 }
+
+output "ses_access_key" {
+  value       = aws_iam_access_key.ses_user_key.id
+  description = "Access Key ID for SES user"
+  sensitive   = true
+}
+
+output "ses_secret_key" {
+  value       = aws_iam_access_key.ses_user_key.secret
+  description = "Secret Access Key for SES user"
+  sensitive   = true
+}
