2e6c307f259aa34cdaa5aad1365ce8431fde405a
Christian Carlsson <hello@crlssn.com>
2024-12-04 19:41:55 +0000

    remove dkim

--- infrastructure/email.tf
+++ infrastructure/email.tf
@@ -13,12 +13,3 @@ resource "aws_route53_record" "ses_verification" {
 resource "aws_ses_domain_dkim" "getstronger" {
   domain = aws_ses_domain_identity.getstronger.domain
 }
-
-resource "aws_route53_record" "dkim" {
-  count   = length(aws_ses_domain_dkim.getstronger.dkim_tokens)
-  zone_id = aws_route53_zone.getstronger_pro.zone_id
-  name    = "${aws_ses_domain_dkim.getstronger.dkim_tokens[count.index]}._domainkey.getstronger.pro"
-  type    = "CNAME"
-  ttl     = 600
-  records = ["${aws_ses_domain_dkim.getstronger.dkim_tokens[count.index]}.dkim.amazonses.com"]
-}
