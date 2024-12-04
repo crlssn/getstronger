9bc8953105711f724cb1b7f59882766efce5b352
Christian Carlsson <hello@crlssn.com>
2024-12-04 19:28:08 +0000

    tf: email service

---
+++ infrastructure/email.tf
@@ -0,0 +1,11 @@
+resource "aws_ses_domain_identity" "getstronger" {
+  domain = "getstronger.pro"
+}
+
+resource "aws_route53_record" "ses_verification" {
+  zone_id = aws_route53_zone.getstronger_pro.zone_id
+  name    = "_amazonses.getstronger.pro"
+  type    = "TXT"
+  ttl     = 600
+  records = [aws_ses_domain_identity.getstronger.verification_token]
+}
