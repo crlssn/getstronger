764e73da6cc411c9284243d945dc9b9db7f8e08c
Christian Carlsson <hello@crlssn.com>
2024-12-04 19:47:57 +0000

    iam policy

--- infrastructure/email.tf
+++ infrastructure/email.tf
@@ -30,3 +30,19 @@ resource "aws_route53_record" "spf" {
   ttl     = 600
   records = ["v=spf1 include:amazonses.com ~all"]
 }
+
+resource "aws_iam_policy" "ses_send_email" {
+  name        = "SES_Send_Email_GetStronger_Pro"
+  description = "Allows sending emails via SES for getstronger.pro"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "ses:SendEmail",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
