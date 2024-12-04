6132f2603c012dec9842ea63a6228269be898cb7
Christian Carlsson <hello@crlssn.com>
2024-12-04 19:37:54 +0000

    configure dkim and spf

--- infrastructure/email.tf
+++ infrastructure/email.tf
@@ -9,3 +9,25 @@ resource "aws_route53_record" "ses_verification" {
   ttl     = 600
   records = [aws_ses_domain_identity.getstronger.verification_token]
 }
+
+resource "aws_ses_domain_dkim" "getstronger" {
+  domain = aws_ses_domain_identity.getstronger.domain
+}
+
+resource "aws_route53_record" "dkim" {
+  for_each = toset(aws_ses_domain_dkim.getstronger.dkim_tokens)
+  zone_id  = aws_route53_zone.getstronger_pro.zone_id
+  name     = "${each.value}._domainkey.getstronger.pro"
+  type     = "CNAME"
+  ttl      = 600
+  records  = ["${each.value}.dkim.amazonses.com"]
+}
+
+resource "aws_route53_record" "spf" {
+  zone_id = aws_route53_zone.getstronger_pro.zone_id
+  name    = "getstronger.pro"
+  type    = "TXT"
+  ttl     = 600
+  records = ["v=spf1 include:amazonses.com ~all"]
+}
+
