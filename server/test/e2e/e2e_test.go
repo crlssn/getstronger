1095afa0f51628daa332a3471ade9017f9e27166
Christian Carlsson <hello@crlssn.com>
2024-12-14 17:27:44 +0000

    checkpoint

--- server/test/e2e/e2e_test.go
+++ server/test/e2e/e2e_test.go
@@ -4,15 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"net/http"
 	"testing"
 
-	"connectrpc.com/connect"
-	"github.com/brianvoe/gofakeit/v7"
 	"github.com/bufbuild/protovalidate-go"
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/require"
-	"github.com/volatiletech/sqlboiler/v4/boil"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 
@@ -21,9 +17,7 @@ import (
 	"github.com/crlssn/getstronger/server/pkg/cookies"
 	"github.com/crlssn/getstronger/server/pkg/email"
 	"github.com/crlssn/getstronger/server/pkg/jwt"
-	"github.com/crlssn/getstronger/server/pkg/orm"
 	v1 "github.com/crlssn/getstronger/server/pkg/proto/api/v1"
-	"github.com/crlssn/getstronger/server/pkg/proto/api/v1/apiv1connect"
 	"github.com/crlssn/getstronger/server/pkg/repo"
 	"github.com/crlssn/getstronger/server/pkg/stream"
 	"github.com/crlssn/getstronger/server/pkg/test/testdb"
@@ -71,85 +65,18 @@ func TestE2E(t *testing.T) {
 
 	saga := newSaga(db, cfg)
 	saga.
-		Signup(ctx, func(res *v1.SignupResponse) {}).
+		Signup(ctx, func(_ *v1.SignupResponse) {}).
 		Login(ctx, func(res *v1.LoginResponse) {
 			require.NotEmpty(t, res.AccessToken)
+		}).
+		CreateExercise(ctx, func(res *v1.CreateExerciseResponse) {
+			require.NotEmpty(t, res.Id)
+		}).
+		ListExercises(ctx, func(res *v1.ListExercisesResponse) {
+			require.Len(t, res.Exercises, 1)
 		})
 
-	baseURL := fmt.Sprintf("https://localhost:%s", cfg.Server.Port)
-
-	email := gofakeit.Email()
-	password := gofakeit.Password(true, true, true, true, true, 6)
-
-	authClient := apiv1connect.NewAuthServiceClient(http.DefaultClient, baseURL)
-	_, err := authClient.Signup(ctx, &connect.Request[v1.SignupRequest]{
-		Msg: &v1.SignupRequest{
-			Email:                email,
-			Password:             password,
-			PasswordConfirmation: password,
-			FirstName:            gofakeit.FirstName(),
-			LastName:             gofakeit.LastName(),
-		},
-	})
-	require.NoError(t, err)
-
-	auth, err := orm.Auths(orm.AuthWhere.Email.EQ(email)).One(ctx, db)
-	require.NoError(t, err)
-
-	auth.EmailVerified = true
-	_, err = auth.Update(ctx, db, boil.Whitelist(orm.AuthColumns.EmailVerified))
-	require.NoError(t, err)
-
-	resLogin, err := authClient.Login(ctx, &connect.Request[v1.LoginRequest]{
-		Msg: &v1.LoginRequest{
-			Email:    email,
-			Password: password,
-		},
-	})
-	require.NoError(t, err)
-
-	client := &http.Client{
-		Transport: &authTransport{
-			accessToken:  resLogin.Msg.AccessToken,
-			roundTripper: http.DefaultTransport,
-		},
-	}
-
-	exerciseClient := apiv1connect.NewExerciseServiceClient(client, baseURL)
-	resCreateExercise, err := exerciseClient.CreateExercise(ctx, &connect.Request[v1.CreateExerciseRequest]{
-		Msg: &v1.CreateExerciseRequest{
-			Name:  gofakeit.RandomString([]string{"Bench Press", "Deadlifts", "Squats"}),
-			Label: "",
-		},
-	})
-	require.NoError(t, err)
-
-	resListExercises, err := exerciseClient.ListExercises(ctx, &connect.Request[v1.ListExercisesRequest]{
-		Msg: &v1.ListExercisesRequest{
-			Name:        "",
-			ExerciseIds: []string{resCreateExercise.Msg.GetId()},
-			Pagination: &v1.PaginationRequest{
-				PageLimit: 100,
-				PageToken: nil,
-			},
-		},
-	})
-	require.NoError(t, err)
-	require.Len(t, resListExercises.Msg.GetExercises(), 1)
-
-	if err = app.Stop(ctx); err != nil {
+	if err := app.Stop(ctx); err != nil {
 		panic(err)
 	}
-}
-
-type authTransport struct {
-	accessToken  string
-	roundTripper http.RoundTripper
-}
-
-func (a *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	clonedReq := req.Clone(req.Context())
-	clonedReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", a.accessToken))
-
-	return a.roundTripper.RoundTrip(clonedReq)
 }
--- server/test/e2e/saga.go
+++ server/test/e2e/saga.go
@@ -8,6 +8,7 @@ import (
 
 	"connectrpc.com/connect"
 	"github.com/brianvoe/gofakeit/v7"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 
 	"github.com/crlssn/getstronger/server/pkg/config"
@@ -19,13 +20,7 @@ import (
 type Saga struct {
 	db      *sql.DB
 	auth    *auth
-	config  *config.Config
-	clients clients
-}
-
-type clients struct {
-	auth     apiv1connect.AuthServiceClient
-	exercise apiv1connect.ExerciseServiceClient
+	baseURL string
 }
 
 type auth struct {
@@ -35,23 +30,44 @@ type auth struct {
 }
 
 func newSaga(db *sql.DB, config *config.Config) *Saga {
-	baseURL := fmt.Sprintf("https://localhost:%s", config.Server.Port)
 	return &Saga{
-		db: db,
+		db:      db,
+		baseURL: fmt.Sprintf("https://localhost:%s", config.Server.Port),
 		auth: &auth{
 			email:    gofakeit.Email(),
 			password: gofakeit.Password(true, true, true, true, true, 6),
 		},
-		config: config,
-		clients: clients{
-			auth: apiv1connect.NewAuthServiceClient(http.DefaultClient, baseURL),
-			//exercise: apiv1connect.NewExerciseServiceClient(http.DefaultClient, baseURL),
+	}
+}
+
+func (s *Saga) client() *http.Client {
+	if s.auth.accessToken == "" {
+		return http.DefaultClient
+	}
+
+	return &http.Client{
+		Transport: &clientTransport{
+			auth:         s.auth,
+			roundTripper: http.DefaultTransport,
 		},
 	}
 }
 
+type clientTransport struct {
+	auth         *auth
+	roundTripper http.RoundTripper
+}
+
+func (a *clientTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	r := req.Clone(req.Context())
+	r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", a.auth.accessToken))
+
+	return a.roundTripper.RoundTrip(r)
+}
+
 func (s *Saga) Signup(ctx context.Context, f func(res *v1.SignupResponse)) *Saga {
-	res, err := s.clients.auth.Signup(ctx, &connect.Request[v1.SignupRequest]{
+	client := apiv1connect.NewAuthServiceClient(s.client(), s.baseURL)
+	res, err := client.Signup(ctx, &connect.Request[v1.SignupRequest]{
 		Msg: &v1.SignupRequest{
 			Email:                s.auth.email,
 			Password:             s.auth.password,
@@ -74,13 +90,22 @@ func (s *Saga) Signup(ctx context.Context, f func(res *v1.SignupResponse)) *Saga
 		panic(err)
 	}
 
+	exists, err := a.User().Exists(ctx, s.db)
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		panic("user does not exist")
+	}
+
 	f(res.Msg)
 
 	return s
 }
 
 func (s *Saga) Login(ctx context.Context, f func(res *v1.LoginResponse)) *Saga {
-	res, err := s.clients.auth.Login(ctx, &connect.Request[v1.LoginRequest]{
+	client := apiv1connect.NewAuthServiceClient(s.client(), s.baseURL)
+	res, err := client.Login(ctx, &connect.Request[v1.LoginRequest]{
 		Msg: &v1.LoginRequest{
 			Email:    s.auth.email,
 			Password: s.auth.password,
@@ -95,5 +120,43 @@ func (s *Saga) Login(ctx context.Context, f func(res *v1.LoginResponse)) *Saga {
 	f(res.Msg)
 
 	return s
+}
 
+func (s *Saga) CreateExercise(ctx context.Context, f func(res *v1.CreateExerciseResponse)) *Saga {
+	spew.Dump(s.auth.accessToken)
+	client := apiv1connect.NewExerciseServiceClient(s.client(), s.baseURL)
+	res, err := client.CreateExercise(ctx, &connect.Request[v1.CreateExerciseRequest]{
+		Msg: &v1.CreateExerciseRequest{
+			Name:  gofakeit.RandomString([]string{"Bench Press", "Deadlifts", "Squats"}),
+			Label: "",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	f(res.Msg)
+
+	return s
+}
+
+func (s *Saga) ListExercises(ctx context.Context, f func(res *v1.ListExercisesResponse)) *Saga {
+	client := apiv1connect.NewExerciseServiceClient(s.client(), s.baseURL)
+	res, err := client.ListExercises(ctx, &connect.Request[v1.ListExercisesRequest]{
+		Msg: &v1.ListExercisesRequest{
+			Name:        "",
+			ExerciseIds: nil,
+			Pagination: &v1.PaginationRequest{
+				PageLimit: 100,
+				PageToken: nil,
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	f(res.Msg)
+
+	return s
 }
