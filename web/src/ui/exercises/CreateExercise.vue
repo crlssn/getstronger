d4bebaa5ca762d079f1a7faa04b5b2623a815307
Christian Carlsson <hello@crlssn.com>
2024-12-15 22:36:52 +0000

    checkpoint

--- web/src/ui/exercises/CreateExercise.vue
+++ web/src/ui/exercises/CreateExercise.vue
@@ -1,25 +1,15 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAlertStore } from '@/stores/alerts'
 import { createExercise } from '@/http/requests'
 import AppList from '@/ui/components/AppList.vue'
 import AppListItemInput from '@/ui/components/AppListItemInput.vue'
 import { type CreateExerciseRequest } from '@/proto/api/v1/exercise_service_pb'
-import { useActionButton } from '@/stores/actionButton.ts'
-import { CheckIcon } from '@heroicons/vue/24/outline'
 import AppButton from '@/ui/components/AppButton.vue'
 
 const router = useRouter()
 const alertStore = useAlertStore()
-const actionButton = useActionButton()
-
-onMounted(() => {
-  actionButton.set({
-    icon: CheckIcon,
-    action: onSubmit,
-  })
-})
 
 const req = ref<CreateExerciseRequest>({
   $typeName: 'api.v1.CreateExerciseRequest',
