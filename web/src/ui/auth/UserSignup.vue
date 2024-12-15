05e674c74b7cc8825a67009fcb5d7b654371373e
Christian Carlsson <hello@crlssn.com>
2024-12-15 22:14:42 +0000

    checkpoint

--- web/src/ui/auth/UserSignup.vue
+++ web/src/ui/auth/UserSignup.vue
@@ -23,89 +23,87 @@ const onSignup = async () => {
 </script>
 
 <template>
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" method="POST" @submit.prevent="onSignup">
-      <div>
-        <label for="email" class="block  font-medium text-gray-900">First name</label>
-        <div class="mt-2">
-          <input
-            id="firstname"
-            v-model="req.firstName"
-            name="firstname"
-            type="text"
-            required
-            class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 "
-          />
-        </div>
+  <form class="space-y-6" method="POST" @submit.prevent="onSignup">
+    <div>
+      <label for="email" class="block  font-medium text-gray-900">First name</label>
+      <div class="mt-2">
+        <input
+          id="firstname"
+          v-model="req.firstName"
+          name="firstname"
+          type="text"
+          required
+          class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 "
+        />
       </div>
+    </div>
 
-      <div>
-        <label for="email" class="block  font-medium text-gray-900">Last name</label>
-        <div class="mt-2">
-          <input v-model="req.lastName" name="lastname" type="text" required />
-        </div>
+    <div>
+      <label for="email" class="block  font-medium text-gray-900">Last name</label>
+      <div class="mt-2">
+        <input v-model="req.lastName" name="lastname" type="text" required />
       </div>
+    </div>
 
-      <div>
-        <label for="email" class="block  font-medium text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input
-            id="email"
-            v-model="req.email"
-            name="email"
-            type="email"
-            autocomplete="email"
-            required
-          />
-        </div>
+    <div>
+      <label for="email" class="block  font-medium text-gray-900">Email address</label>
+      <div class="mt-2">
+        <input
+          id="email"
+          v-model="req.email"
+          name="email"
+          type="email"
+          autocomplete="email"
+          required
+        />
       </div>
+    </div>
 
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block  font-medium text-gray-900">Password</label>
-        </div>
-        <div class="mt-2">
-          <input
-            id="password"
-            v-model="req.password"
-            name="password"
-            type="password"
-            autocomplete="current-password"
-            required
-          />
-        </div>
+    <div>
+      <div class="flex items-center justify-between">
+        <label for="password" class="block  font-medium text-gray-900">Password</label>
       </div>
-
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block  font-medium text-gray-900">
-            Confirm Password
-          </label>
-        </div>
-        <div class="mt-2">
-          <input
-            id="passwordConfirmation"
-            v-model="req.passwordConfirmation"
-            name="passwordConfirmation"
-            type="password"
-            autocomplete="current-password"
-            required
-          />
-        </div>
+      <div class="mt-2">
+        <input
+          id="password"
+          v-model="req.password"
+          name="password"
+          type="password"
+          autocomplete="current-password"
+          required
+        />
       </div>
+    </div>
 
-      <div>
-        <AppButton type="submit" colour="primary"> Sign up</AppButton>
+    <div>
+      <div class="flex items-center justify-between">
+        <label for="password" class="block  font-medium text-gray-900">
+          Confirm Password
+        </label>
       </div>
-    </form>
+      <div class="mt-2">
+        <input
+          id="passwordConfirmation"
+          v-model="req.passwordConfirmation"
+          name="passwordConfirmation"
+          type="password"
+          autocomplete="current-password"
+          required
+        />
+      </div>
+    </div>
 
-    <p class="mt-6 text-center  text-gray-400">
-      Already a member?
-      <RouterLink to="/login" class="font-semibold text-indigo-600 hover:text-indigo-500">
-        Login
-      </RouterLink>
-    </p>
-  </div>
+    <div>
+      <AppButton type="submit" colour="primary"> Sign up</AppButton>
+    </div>
+  </form>
+
+  <p class="mt-6 text-center  text-gray-400">
+    Already a member?
+    <RouterLink to="/login" class="font-semibold text-indigo-600 hover:text-indigo-500">
+      Login
+    </RouterLink>
+  </p>
 </template>
 
 <style scoped>
