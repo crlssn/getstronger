d1cfc2bc46c3c336368bd548cf6c8fbbadf8c937
Christian Carlsson <hello@crlssn.com>
2024-12-15 21:41:10 +0000

    refact: web: desktop layout

--- web/src/ui/components/AppNavTabs.vue
+++ web/src/ui/components/AppNavTabs.vue
@@ -7,19 +7,30 @@ const navTabs = useNavTabs()
 </script>
 
 <template>
-  <nav class="flex bg-white">
-    <RouterLink
-      v-for="tab in navTabs.tabs"
-      :key="tab.name"
-      :to="tab.href"
-      :class="route.fullPath === tab.href ? 'active' : ''"
-    >
-      {{ tab.name }}
-    </RouterLink>
+  <nav>
+    <div class="container">
+      <RouterLink
+        v-for="tab in navTabs.tabs"
+        :key="tab.name"
+        :to="tab.href"
+        :class="route.fullPath === tab.href ? 'active' : ''"
+      >
+        {{ tab.name }}
+      </RouterLink>
+    </div>
   </nav>
 </template>
 
 <style scoped>
+nav {
+  @apply bg-white w-full border-b-2 border-gray-200;
+
+  .container {
+    @apply flex max-w-4xl mx-auto;
+    margin-bottom: -2px;
+  }
+}
+
 a {
   @apply w-full border-b-2 pb-4 text-center text-xs font-bold uppercase;
   @apply border-b-2 border-gray-200 text-gray-500 hover:border-gray-300 hover:text-gray-700;
