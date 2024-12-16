588a1aec6e0798cadba2b93155ee5ecaa0666b9f
Christian Carlsson <hello@crlssn.com>
2024-12-16 16:34:54 +0000

    checkpoint

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 import AppDashboard from '@/ui/components/AppDashboard.vue'
+import GuestView from '@/ui/components/GuestView.vue'
 
 const authStore = useAuthStore()
 </script>
 
 <template>
   <AppDashboard v-if="authStore.authorised" />
-  <main v-else>
-    <RouterView />
-  </main>
+  <GuestView v-else/>
 </template>
 
-<style scoped>
-main {
-  @apply flex min-h-full flex-col justify-center px-4 max-w-4xl mx-auto;
-}
-</style>
+<style scoped></style>
---
+++ web/src/ui/components/GuestView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <div class="container">
+      <p><span class="font-semibold">Get</span><span class="font-bold">Stronger</span></p>
+    </div>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+nav {
+  @apply fixed w-full top-0 z-30  border-gray-200 bg-white h-16;
+
+  .container {
+    @apply flex items-center justify-center max-w-4xl mx-auto px-4 h-16 border-b-2 border-gray-200;
+    p {
+      @apply text-2xl;
+    }
+  }
+}
+
+main {
+  @apply flex min-h-full flex-col justify-center px-4 max-w-4xl mx-auto pt-20 pb-8;
+}
+</style>
