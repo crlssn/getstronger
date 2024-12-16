bdcd93cdacd22ccdd69f2d903c449021043d779c
Christian Carlsson <hello@crlssn.com>
2024-12-16 16:02:32 +0000

    checkpoint

--- web/src/ui/auth/UserLogout.vue
+++ web/src/ui/auth/UserLogout.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { logout as logoutRequest } from '@/http/requests.ts'
+import { logout } from '@/http/requests.ts'
 import { useAuthStore } from '@/stores/auth.ts'
 import { useNotificationStore } from '@/stores/notifications.ts'
 import router from '@/router/router.ts'
 
 onMounted(async () => {
-  await logoutRequest()
+  await logout()
   const authStore = useAuthStore()
   authStore.logout()
 
