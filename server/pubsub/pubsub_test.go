f0210aaa6fcda18f00fb26e7ad69d1b3a1189f9a
Christian Carlsson <hello@crlssn.com>
2024-12-20 13:48:44 +0000

    checkpoint

--- server/pubsub/pubsub_test.go
+++ server/pubsub/pubsub_test.go
@@ -6,11 +6,13 @@ import (
 
 	"github.com/lib/pq"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/mock/gomock"
 	"go.uber.org/zap"
 
 	"github.com/crlssn/getstronger/server/gen/orm"
 	"github.com/crlssn/getstronger/server/pubsub"
 	"github.com/crlssn/getstronger/server/pubsub/handlers"
+	"github.com/crlssn/getstronger/server/pubsub/handlers/mocks"
 	"github.com/crlssn/getstronger/server/testing/container"
 )
 
@@ -18,6 +20,11 @@ type pubSubSuite struct {
 	suite.Suite
 
 	pubSub *pubsub.PubSub
+
+	mocks struct {
+		handler    *mock_handlers.MockHandler
+		controller *gomock.Controller
+	}
 }
 
 func (s *pubSubSuite) SetupSuite() {
@@ -30,10 +37,15 @@ func (s *pubSubSuite) SetupSuite() {
 		Listener: pq.NewListener(c.Connection, time.Second, time.Minute, nil),
 	})
 
-	s.pubSub.Subscribe(map[orm.EventTopic]handlers.Handler{
-		orm.EventTopicFollowedUser:  handlers.NewFollowedUser(),
-		orm.EventTopicRequestTraced: handlers.NewRequestTraced(),
+	s.mocks.controller = gomock.NewController(s.T())
+	s.mocks.handler = mock_handlers.NewMockHandler(s.mocks.controller)
+
+	err := s.pubSub.Subscribe(map[orm.EventTopic]handlers.Handler{
+		orm.EventTopicFollowedUser:         s.mocks.handler,
+		orm.EventTopicRequestTraced:        s.mocks.handler,
+		orm.EventTopicWorkoutCommentPosted: s.mocks.handler,
 	})
+	s.Require().NoError(err)
 
 	s.T().Cleanup(func() {
 		if err := c.Terminate(ctx); err != nil {
