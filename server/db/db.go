a5cb71aeef8c97698e5f827d32b9ba322c090c5d
Christian Carlsson <hello@crlssn.com>
2024-12-20 17:39:59 +0000

    fix: pubsub: prod db connection

--- server/db/db.go
+++ server/db/db.go
@@ -19,10 +19,10 @@ func New(c *config.Config) (*sql.DB, error) {
 }
 
 func ConnectionString(c *config.Config) string {
-	sslMode := "prefer"
+	sslMode := ""
 	if c.Env == "development" {
-		sslMode = "disable"
+		sslMode = "?sslmode=disable"
 	}
 
-	return fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", c.DB.User, c.DB.Password, net.JoinHostPort(c.DB.Host, c.DB.Port), c.DB.Name, sslMode)
+	return fmt.Sprintf("postgresql://%s:%s@%s/%s%s", c.DB.User, c.DB.Password, net.JoinHostPort(c.DB.Host, c.DB.Port), c.DB.Name, sslMode)
 }
