bb1a9396f67e73355a681897ea6b70f4a040a76a
Christian Carlsson <hello@crlssn.com>
2024-12-22 18:31:52 +0000

    checkpoint

--- server/rpc/server/module.go
+++ server/rpc/server/module.go
@@ -1,25 +1,15 @@
 package server
 
 import (
-	"context"
-	"crypto/tls"
-	"errors"
-	"fmt"
-	"log"
 	"net/http"
-	"time"
 
 	"connectrpc.com/connect"
 	"go.uber.org/fx"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 
-	"github.com/crlssn/getstronger/server/config"
 	"github.com/crlssn/getstronger/server/gen/proto/api/v1/apiv1connect"
 	handlers "github.com/crlssn/getstronger/server/rpc/handlers/v1"
 	"github.com/crlssn/getstronger/server/rpc/interceptors"
 	"github.com/crlssn/getstronger/server/rpc/middlewares"
-	"github.com/crlssn/getstronger/server/stream"
 )
 
 func Module() fx.Option {
@@ -44,53 +34,6 @@ func Module() fx.Option {
 			})
 		}),
 	))
-}
-
-type Server struct {
-	conn     *stream.Conn
-	server   *http.Server
-	keyPath  string
-	certPath string
-}
-
-func newServer(config *config.Config, mux *http.ServeMux, conn *stream.Conn) *Server {
-	return &Server{
-		conn:     conn,
-		keyPath:  config.Server.KeyPath,
-		certPath: config.Server.CertPath,
-		server: &http.Server{
-			Addr:         fmt.Sprintf(":%s", config.Server.Port),
-			Handler:      h2c.NewHandler(mux, &http2.Server{}),
-			ReadTimeout:  10 * time.Second, //nolint:mnd
-			WriteTimeout: 0,
-			IdleTimeout:  120 * time.Second, //nolint:mnd
-			TLSConfig: &tls.Config{
-				MinVersion: tls.VersionTLS12,
-			},
-		},
-	}
-}
-
-func (s *Server) ListenAndServe(_ context.Context) error {
-	go func() {
-		if err := s.listenAndServe(); err != nil {
-			if errors.Is(err, http.ErrServerClosed) {
-				return
-			}
-			log.Fatalf("listen and serve: %v", err)
-		}
-	}()
-	return nil
-}
-
-func (s *Server) listenAndServe() error {
-	s.server.RegisterOnShutdown(s.conn.Cancel)
-
-	if s.certPath == "" && s.keyPath == "" {
-		return s.server.ListenAndServe() //nolint:wrapcheck
-	}
-
-	return s.server.ListenAndServeTLS(s.certPath, s.keyPath) //nolint:wrapcheck
 }
 
 type Handlers struct {
---
+++ server/rpc/server/server.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	"github.com/crlssn/getstronger/server/config"
+	"github.com/crlssn/getstronger/server/stream"
+)
+
+type Server struct {
+	conn     *stream.Conn
+	server   *http.Server
+	keyPath  string
+	certPath string
+}
+
+func newServer(config *config.Config, mux *http.ServeMux, conn *stream.Conn) *Server {
+	return &Server{
+		conn:     conn,
+		keyPath:  config.Server.KeyPath,
+		certPath: config.Server.CertPath,
+		server: &http.Server{
+			Addr:         fmt.Sprintf(":%s", config.Server.Port),
+			Handler:      h2c.NewHandler(mux, &http2.Server{}),
+			ReadTimeout:  10 * time.Second, //nolint:mnd
+			WriteTimeout: 0,
+			IdleTimeout:  120 * time.Second, //nolint:mnd
+			TLSConfig: &tls.Config{
+				MinVersion: tls.VersionTLS12,
+			},
+		},
+	}
+}
+
+func (s *Server) ListenAndServe(_ context.Context) error {
+	go func() {
+		if err := s.listenAndServe(); err != nil {
+			if errors.Is(err, http.ErrServerClosed) {
+				return
+			}
+			log.Fatalf("listen and serve: %v", err)
+		}
+	}()
+	return nil
+}
+
+func (s *Server) listenAndServe() error {
+	s.server.RegisterOnShutdown(s.conn.Cancel)
+
+	if s.certPath == "" && s.keyPath == "" {
+		return s.server.ListenAndServe() //nolint:wrapcheck
+	}
+
+	return s.server.ListenAndServeTLS(s.certPath, s.keyPath) //nolint:wrapcheck
+}
