1c52aa41d495fd96598c83d46fafdbbedbee14f4
Christian Carlsson <hello@crlssn.com>
2025-01-10 21:41:32 +0000

    remove quotes

--- infrastructure/migrations.tf
+++ infrastructure/migrations.tf
@@ -1,24 +1,24 @@
 moved {
-  from = "aws_cloudwatch_log_group.backend_logs"
-  to   = "module.cloudwatch.aws_cloudwatch_log_group.log_group"
+  from = aws_cloudwatch_log_group.backend_logs
+  to   = module.cloudwatch.aws_cloudwatch_log_group.log_group
 }
 
 moved {
-  from = "aws_iam_role.ec2_cloudwatch_role"
-  to   = "module.cloudwatch.aws_iam_role.role"
+  from = aws_iam_role.ec2_cloudwatch_role
+  to   = module.cloudwatch.aws_iam_role.role
 }
 
 moved {
-  from = "aws_iam_policy.cloudwatch_logs_policy"
-  to   = "module.cloudwatch.aws_iam_policy.log_policy"
+  from = aws_iam_policy.cloudwatch_logs_policy
+  to   = module.cloudwatch.aws_iam_policy.log_policy
 }
 
 moved {
-  from = "aws_iam_role_policy_attachment.attach_policy"
-  to   = "module.cloudwatch.aws_iam_role_policy_attachment.policy_attachment"
+  from = aws_iam_role_policy_attachment.attach_policy
+  to   = module.cloudwatch.aws_iam_role_policy_attachment.policy_attachment
 }
 
 moved {
-  from = "aws_iam_instance_profile.ec2_instance_profile"
-  to   = "module.cloudwatch.aws_iam_instance_profile.instance_profile"
+  from = aws_iam_instance_profile.ec2_instance_profile
+  to   = module.cloudwatch.aws_iam_instance_profile.instance_profile
 }
