ffe1a7ea03033c30e5412020b3b0dd005f9f9cb8
Christian Carlsson <hello@crlssn.com>
2025-01-10 19:37:24 +0000

    feat: web: don't alert unkown errors (#380)

--- web/src/http/requests.ts
+++ web/src/http/requests.ts
@@ -490,7 +490,12 @@ const tryCatch = async <T>(fn: () => Promise<T>): Promise<T | void> => {
       }
 
       // DEBT: Filter out some error codes to alert the user until we have a better way to handle them.
-      const ignoredCodes: Code[] = [Code.Unauthenticated, Code.Unavailable, Code.Canceled]
+      const ignoredCodes: Code[] = [
+        Code.Unknown,
+        Code.Canceled,
+        Code.Unavailable,
+        Code.Unauthenticated,
+      ]
       if (!ignoredCodes.includes(error.code)) {
         alert(error.message)
         return
