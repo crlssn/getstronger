7e911f29a24ddb13397eb6c45729e26cdcd86157
Christian Carlsson <hello@crlssn.com>
2025-01-10 21:22:04 +0000

    use module role name

--- infrastructure/modules/cloudwatch/main.tf
+++ infrastructure/modules/cloudwatch/main.tf
@@ -41,3 +41,7 @@ resource "aws_iam_instance_profile" "instance_profile" {
 output "instance_profile_name" {
   value = aws_iam_instance_profile.instance_profile.name
 }
+
+output "role_name" {
+    value = aws_iam_role.role.name
+}
