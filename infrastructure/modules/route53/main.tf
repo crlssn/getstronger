55d477c6b9cfa92d821be29c6048561bea265d66
Christian Carlsson <hello@crlssn.com>
2025-01-11 22:53:24 +0000

    checkpoint

--- infrastructure/modules/route53/main.tf
+++ infrastructure/modules/route53/main.tf
@@ -11,13 +11,9 @@ resource "aws_route53_record" "records" {
   ttl     = each.value.ttl
   records = each.value.records
 
-  dynamic "alias" {
-    # Use a dummy list to conditionally include this block.
-    for_each = each.value.records != [] ? ["ok"] : []
-    content {
-      name                   = each.value.alias_name
-      zone_id                = each.value.alias_zone_id
-      evaluate_target_health = each.value.evaluate_target_health
-    }
+  alias {
+    name                   = each.value.alias_name
+    zone_id                = each.value.alias_zone_id
+    evaluate_target_health = each.value.evaluate_target_health
   }
 }
