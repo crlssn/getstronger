28c712b60fa5f0fbe3b4ba53d192e3ab8788f3e6
Christian Carlsson <hello@crlssn.com>
2025-01-11 23:01:57 +0000

    checkpoint

--- infrastructure/modules/route53/main.tf
+++ infrastructure/modules/route53/main.tf
@@ -10,10 +10,9 @@ resource "aws_route53_record" "records" {
   type    = each.value.type
   ttl     = each.value.ttl
   records = each.value.records == [] ? null : each.value.records
-
-  alias {
-    name                   = each.value.alias_name
-    zone_id                = each.value.alias_zone_id
-    evaluate_target_health = each.value.evaluate_target_health
+  alias = each.value.alias == null ? null : {
+    name                   = each.value.alias.name
+    zone_id                = each.value.alias.zone_id
+    evaluate_target_health = each.value.alias.evaluate_target_health
   }
 }
--- infrastructure/modules/route53/variables.tf
+++ infrastructure/modules/route53/variables.tf
@@ -11,11 +11,13 @@ variable "domain" {
 variable "subdomains" {
   description = "A map of subdomains and their configurations"
   type = map(object({
-    type                   = string
-    ttl                    = number
-    records                = list(string)
-    alias_name             = string
-    alias_zone_id          = string
-    evaluate_target_health = bool
+    type    = string
+    ttl     = number
+    records = list(string)
+    alias = object({
+      name                   = string
+      zone_id                = string
+      evaluate_target_health = bool
+    })
   }))
 }
