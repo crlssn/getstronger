7448529b7a8d2a8b838e6036f12efaaf215ca089
Christian Carlsson <hello@crlssn.com>
2025-01-15 13:01:12 +0000

    exercise: create exercise

--- server/rpc/handlers/v1/exercise_test.go
+++ server/rpc/handlers/v1/exercise_test.go
@@ -11,6 +11,7 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 
+	"github.com/crlssn/getstronger/server/gen/orm"
 	v1 "github.com/crlssn/getstronger/server/gen/proto/api/v1"
 	"github.com/crlssn/getstronger/server/gen/proto/api/v1/apiv1connect"
 	"github.com/crlssn/getstronger/server/repo"
@@ -55,17 +56,23 @@ func (s *exerciseSuite) TestCreateExercise() {
 	type test struct {
 		name     string
 		req      *connect.Request[v1.CreateExerciseRequest]
+		init     func(t test) context.Context
 		expected expected
 	}
 
 	tests := []test{
 		{
-			name: "valid_request",
+			name: "ok_exercise_created",
 			req: &connect.Request[v1.CreateExerciseRequest]{
 				Msg: &v1.CreateExerciseRequest{
-					Name:  "Bench Press",
-					Label: "",
+					Name:  "Name",
+					Label: "Label",
 				},
+			},
+			init: func(t test) context.Context {
+				user := s.factory.NewUser()
+				ctx := xcontext.WithLogger(context.Background(), zap.NewExample())
+				return xcontext.WithUserID(ctx, user.ID)
 			},
 			expected: expected{
 				err: nil,
@@ -73,25 +80,28 @@ func (s *exerciseSuite) TestCreateExercise() {
 		},
 	}
 
-	user := s.factory.NewUser()
-	ctx := xcontext.WithUserID(context.Background(), user.ID)
-	ctx = xcontext.WithLogger(ctx, zap.NewExample())
-
 	for _, t := range tests {
 		s.Run(t.name, func() {
+			ctx := t.init(t)
+
 			res, err := s.handler.CreateExercise(ctx, t.req)
 			if t.expected.err != nil {
 				s.Require().Nil(res)
 				s.Require().Error(err)
-				s.Require().ErrorIs(err, t.expected.err)
-
+				s.Require().Equal(t.expected.err.Error(), err.Error())
 				return
 			}
 
 			s.Require().NoError(err)
 			s.Require().NotNil(res)
+
 			_, err = uuid.Parse(res.Msg.GetId())
 			s.Require().NoError(err)
+
+			exercise, err := orm.FindExercise(ctx, s.testContainer.DB, res.Msg.GetId())
+			s.Require().NoError(err)
+			s.Require().NotNil(exercise)
+			s.Require().Equal(t.req.Msg.GetLabel(), exercise.SubTitle.String)
 		})
 	}
 }
