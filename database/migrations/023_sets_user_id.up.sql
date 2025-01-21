149686bcdc1b46f276058e6fb465b36882fa4511
Christian Carlsson <hello@crlssn.com>
2025-01-21 16:50:15 +0000

    trigger

--- database/migrations/023_sets_user_id.up.sql
+++ database/migrations/023_sets_user_id.up.sql
@@ -1,3 +1,4 @@
 ALTER TABLE getstronger.sets ADD COLUMN user_id UUID NULL;
 UPDATE getstronger.sets SET user_id = (SELECT user_id FROM getstronger.workouts WHERE getstronger.workouts.id = getstronger.sets.workout_id);
 ALTER TABLE getstronger.sets ALTER COLUMN user_id SET NOT NULL;
+
