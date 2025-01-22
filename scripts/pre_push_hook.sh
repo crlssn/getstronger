edee4710db9e877bdc88dad25fdd39a7757b76f9
Christian Carlsson <hello@crlssn.com>
2025-01-22 11:59:59 +0000

    align icons

--- scripts/pre_push_hook.sh
+++ scripts/pre_push_hook.sh
@@ -9,21 +9,21 @@ echo "Running pre-push hook..."
 echo "Formatting code..."
 make format > /dev/null
 if [[ $(git status --porcelain) ]]; then
-  echo -e "⚠️${RED}Uncommitted changes found. Aborting push.${NO_COLOUR}"
+  echo -e "⚠️ ${RED}Uncommitted changes found. Aborting push.${NO_COLOUR}"
   exit 1
 fi
 
 echo "Linting code..."
 make lint > /dev/null || {
-    echo -e "${RED}⚠️Linting failed. Aborting push.${NO_COLOUR}"
+    echo -e "⚠️ ${RED}Linting failed. Aborting push.${NO_COLOUR}"
     exit 1
 }
 
 echo "Running tests..."
 make test > /dev/null || {
-    echo -e "${RED}⚠️Tests failed. Aborting push.${NO_COLOUR}"
+    echo -e "⚠️ ${RED}Tests failed. Aborting push.${NO_COLOUR}"
     exit 1
 }
 
-echo -e "${GREEN}✅ All checks passed. Pushing changes...${NO_COLOUR}"
+echo -e "✅ ${GREEN}All checks passed. Pushing changes...${NO_COLOUR}"
 exit 0
