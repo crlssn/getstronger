df8aff1a21bb5e9362330d439f0672f37d78ca0b
Christian Carlsson <hello@crlssn.com>
2025-01-22 11:25:40 +0000

    checkpoint

--- scripts/pre_push_hook.sh
+++ scripts/pre_push_hook.sh
@@ -8,8 +8,8 @@ make format
 
 echo "Checking for uncommitted changes"
 if [[ $(git status --porcelain) ]]; then
-  diff "$(git diff)"
-  echo diff
+  diff=$(git diff)
+  echo "$diff"
   echo "Uncommitted changes found. Aborting push."
   exit 1
 fi
